6377c45c593270561a05482a06b3d4e95a13e830
Mary Goldspink <marygoldspink@gmail.com>
2018-08-29 20:07:25 +0100

    adding defer flag to js file and adding onerror hook as per project feedback

--- src/MyMarker.js
+++ src/MyMarker.js
@@ -10,8 +10,17 @@ import L from 'leaflet'
 // This code was taken from https://github.com/PaulLeCam/react-leaflet/issues/317
 function MyMarker(props) {
     // loading two different marker icons to use when a place selected
-    const customMarker = L.icon({ iconUrl: markerActive })
-    const defaultMarker = L.icon({ iconUrl: marker })
+    // the other properties I copied from the leaflet.js source code.
+    const customMarker = L.icon({ iconUrl: markerActive, iconSize:    [25, 41],
+		iconAnchor:  [12, 41],
+		popupAnchor: [1, -34],
+		tooltipAnchor: [16, -28],
+		shadowSize:  [41, 41]})
+    const defaultMarker = L.icon({ iconUrl: marker, iconSize:    [25, 41],
+		iconAnchor:  [12, 41],
+		popupAnchor: [1, -34],
+		tooltipAnchor: [16, -28],
+		shadowSize:  [41, 41] })
     const position=[props.place.lat, props.place.lng];
     return (
         <Marker position={position} icon={props.place.selected ? customMarker : defaultMarker}>
